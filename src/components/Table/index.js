96818630071cd97925ee0dae1a514586907dbb32
Amaobi Obikobe <obikobea@accessbankplc.com>
2022-01-29 09:16:13 +0100

    complete side nav

---
+++ src/components/Table/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Table.scss';
+
+
+const Table = (props) => {
+  return (
+    <div className='Table'>
+  
+    </div>
+  );
+};
+
+export default Table;
--- src/containers/SideBar/index.js
+++ src/containers/SideBar/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import "./SideBar.scss";
-import Logo from '../../img/logo.svg'
+import './SideBar.scss';
+import Logo from '../../img/logo.svg';
 import NavBar from '../../components/NavBar';
 
-const SideBar = (props) =>{
-
-    return (<div className='SideBar'>
-        <div className='logo'>
-            <img src={Logo} alt='logo' />
+const SideBar = (props) => {
+  return (
+    <div className='SideBar'>
+      <div className='logo'>
+        <img src={Logo} alt='logo' />
+      </div>
+      <NavBar />
+      <div className='user'>
+        <span className='user__image'>
+          <img src='./img1.png' alt='profile pics' />
+        </span>
+        <div className='user__detail'>
+          <span> Nafisa Sh.</span>
+          <span> Support Manager</span>
         </div>
-        <NavBar/>
-    </div>);
-}
+      </div>
+    </div>
+  );
+};
 
 export default SideBar;
